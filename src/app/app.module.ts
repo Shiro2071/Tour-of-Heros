1b2ed6a281a9a53525231cfeb7ef04cbb3374aff
Shiro2071 <philipp.leber@aon.at>
2018-12-17 17:01:05 +0100

    styleguide removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
