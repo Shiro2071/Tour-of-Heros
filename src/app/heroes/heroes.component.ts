b846de9c48f0862f99f562eb704628e9d6e36bc3
Shiro2071 <philipp.leber@aon.at>
2018-12-18 17:54:30 +0100

    cleanup of hero.ts and heroes.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Hero} from './hero'
-import { HEROES } from './mock-heroes';
+import {Hero} from '../hero'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,16 +16,20 @@ export class HeroesComponent implements OnInit {
   //}
   
   selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-   }
+  }
 
-  heroes = HEROES;
-
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
